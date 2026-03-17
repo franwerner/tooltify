0437c3560b372a2bc9602bb6fb92133fb601a38e
Franco Werner <ifrank44445@gmail.com>
2026-03-17 23:51:29 +0000

    feat(core): add SOURCE_PROPERTY_NAME constant and export from index

--- core/index.ts
+++ core/index.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 export * from './server'
+export * from "./common/constant/sourceProperyName.constant"
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export const CLIENT_BUNDLE = path.resolve(__dirname, "./client/client.iife.js");
